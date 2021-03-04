c778ffcaa061bc6b04049e53363077484668452a
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2021-03-04 19:27:51 -0400

    add Job interface

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,4 +1,19 @@
-export default function Card({ job }) {
+interface Job {
+  company: string;
+  logo: string;
+  new: boolean;
+  featured: boolean;
+  position: string;
+  role: string;
+  level: string;
+  postedAt: string;
+  contract: string;
+  location: string;
+  languages: string[];
+  tools: string[];
+}
+
+export default function Card({ job }: { job: Job }) {
   return (
     <div
       id="card"
