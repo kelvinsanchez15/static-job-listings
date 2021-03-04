290fdfdc7fe85e62073658f37b45bae6c5854235
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2021-03-04 11:51:13 -0400

    set font family and color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Spartan', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        cyan: {
+          lighter: 'hsl(180, 52%, 96%)',
+          light: 'hsl(180, 31%, 95%)',
+          DEFAULT: 'hsl(180, 29%, 50%)',
+          dark: 'hsl(180, 8%, 52%)',
+          darkest: 'hsl(180, 14%, 20%)',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
